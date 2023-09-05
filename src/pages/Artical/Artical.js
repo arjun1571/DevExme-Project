e9d688e7ca321835a90d87a1df1767b1ebef884c
Arjun <arjundasback515@gmail.com>
2023-09-05 16:46:41 +0600

    articale page form style cheange

--- src/pages/Artical/Artical.js
+++ src/pages/Artical/Artical.js
@@ -99,7 +99,7 @@ const Artical = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="lg:p-10 md:p-5 p-3 ">
+    <form onSubmit={handleSubmit} className="lg:p-10 md:p-5 p-3 bg-base-300 ">
       <div className="flex md:justify-between justify-around md:border p-2 lg:mx-60  ">
         <div className="">
           <div className="flex items-center">
@@ -107,7 +107,7 @@ const Artical = () => {
             <div className="relative w-full mx-3 ">
               <select name="topic"
                 onChange={(event) => getData(event.target.value)} // Attach onChange to the <select> element
-                className="md:w-48 w-32 p-2.5 rounded-md shadow-sm outline-none appearance-none focus:border-black"
+                className="md:w-48 w-32 p-2.5 rounded-md shadow-lg outline-none appearance-none focus:border-black"
               >
                 {topics.map((topic) => (
                   <option  key={topic.TopicID} value={topic.TopicID}>
@@ -122,7 +122,7 @@ const Artical = () => {
           <div className="flex items-center">
             <p>SubTopic</p>
             <div className="relative w-full lg:max-w-sm mx-3">
-              <select name="subtopic" className="md:w-48 w-32 p-2.5 rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
+              <select name="subtopic" className="md:w-48 w-32 p-2.5 rounded-md shadow-lg outline-none appearance-none focus:border-indigo-600">
                 {filteredTopic?.map((subTopic, index) => (
                   <option key={index}>{subTopic.Name}</option>
                 ))}
@@ -137,7 +137,7 @@ const Artical = () => {
           <input
           name="header"
           required
-            class=" appearance-none border-2-black border-black-200 rounded w-full py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            class=" shadow-lg appearance-none border-2-black border-black-200 rounded w-full py-2 px-4  leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             id="inline-full-name"
             type="text"
             placeholder="Search"
@@ -147,12 +147,12 @@ const Artical = () => {
       <div className="mt-5 lg:mx-60 md:mx-12">
         <p>Abstract</p>
         <div class=" py-3">
-          <textarea name="abstract" class="resize rounded-md w-full md:py-10 py-7"></textarea>
+          <textarea name="abstract" class="shadow-lg resize rounded-md w-full md:py-10 py-7 p-5"></textarea>
         </div>
       </div>
       {/* <Quill></Quill> */}
       <div>
-      <div className='lg:mx-60 bg-white text-black'>
+      <div className='lg:mx-60 bg-white text-black shadow-lg'>
       <ReactQuill
         theme="snow"
         value={value}
@@ -182,7 +182,7 @@ const Artical = () => {
       <div className="mt-5 lg:mx-60 md:mx-12">
         <p>Embed video</p>
         <div class="md:w-3/4 py-3">
-          <textarea name="embed" required class="resize rounded-md w-3/4 md:py-10 py-7"></textarea>
+          <textarea name="embed" required class="resize rounded-md w-3/4 md:py-10 py-7 shadow-lg p-5"></textarea>
         </div>
       </div>
       <div className="mt-5 lg:mx-60 md:mx-12">
@@ -190,7 +190,7 @@ const Artical = () => {
         <div class="mb-3 mt-3">
           <input
         
-            className="relative m-0 block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-400 file:px-3 file:py-[0.32rem] file:text-neutral-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-400 focus:border-primary focus:text-neutral-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-800 dark:file:bg-neutral-00 dark:file:text-neutral-700 dark:focus:border-primary"
+            className=" shadow-lg relative m-0 block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-400 file:px-3 file:py-[0.32rem] file:text-neutral-800 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-400 focus:border-primary focus:text-neutral-800 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-800 dark:file:bg-neutral-00 dark:file:text-neutral-700 dark:focus:border-primary"
             type="file"
             id="formFile"
           />
